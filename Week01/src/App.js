1032d26242d949c9b4032dc8c66ecfec24fd500a
JordanCarlson7 <49733006+JordanCarlson7@users.noreply.github.com>
2022-01-21 17:04:41 -0700

    changes?

--- Week01/src/App.js
+++ Week01/src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import Expenses from "./components/Expenses";
 
@@ -43,49 +42,3 @@ function App() {
 }
 
 export default App;
-=======
-import React from "react";
-import Expenses from "./components/Expenses";
-
-// const APP = () => {}
-function App() {
-  const expenses = [
-    {
-      id: "e1",
-      title: "Toilet Paper",
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
-    {
-      id: "e3",
-      title: "Car Insurance",
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: "e4",
-      title: "New Desk (Wooden)",
-      amount: 450,
-      date: new Date(2021, 5, 12),
-    },
-  ];
-
-    //same things but older style of React or a very literal way to using react
-  // return React.createElement(
-  //   "div",
-  //   {},
-  //   React.createElement("h2", {}, "Lets get started"),
-  //   React.createElement(Expenses, { items: expenses })
-  // );
-
-  return (
-    <div>
-      <h2>Let's get started!</h2>
-      <Expenses items={expenses} />
-    </div>
-  );
-}
-
-export default App;
->>>>>>> d39391126c56a2f7c6fbf3368883d0a67ea12b00
